e96c9da3d15f6d681814baf8b535effe70d0130b
dialsyp <dialsyphax@gmail.com>
2024-12-05 13:45:38 +0100

    <FIX> Maintainability

---
+++ src/auth/authsocial-media/dto/update-authsocial-media.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateAuthsocialMediaDto } from './create-authsocial-media.dto';
+
+export class UpdateAuthsocialMediaDto extends PartialType(
+  CreateAuthsocialMediaDto,
+) {}
